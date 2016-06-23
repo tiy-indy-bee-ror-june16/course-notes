b9c6d61d54b2a02481d7dc4f3e1541096f0f6bd1
Chris Vannoy <chris.vannoy@theironyard.com>
2016-06-23 12:53:49 -0400

    Final in class code

--- week2/day4/hw_review/robot.rb
+++ week2/day4/hw_review/robot.rb
@@ -55,8 +55,47 @@ end
 
 our_class = %w(Isaiah Sean Marie Danny)
 
-robots = our_class.map{ |student| Robot.new(student) }
+class Factory
 
+  def self.build_robots(names)
+    names.map{ |name| Robot.new(name) }
+  end
+
+end
+
+class String
+
+  def piglatinify
+    self.split.map do |word|
+      word[1..-1] + word[0] + "ay"
+    end.join(" ")
+  end
+
+  def reverse
+    self
+  end
+
+end
+
+"banana".piglatinify
+
+class Piglatinify
+
+  def self.change_this(string)
+    string.split.map do |word|
+      word[1..-1] + word[0] + "ay"
+    end.join(" ")
+  end
+
+end
+
+Piglatinify.change_this("banana")
+
+robots = Factory.build_robots(our_class)
+
+robots.each do |robot|
+  puts robot.say_name
+end
 
 robot = Robot.new("Charles")
 puts robot.name
